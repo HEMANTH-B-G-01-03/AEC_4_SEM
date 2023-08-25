e6753fbc7803e432d23ab101643b6c6052f54e77
HEMANTH <110120035+HEMANTH-B-G-01-03@users.noreply.github.com>
2023-08-25 20:11:06 +0530

    Update largestprimefactor.go

--- largestprimefactor.go
+++ largestprimefactor.go
@@ -2,28 +2,31 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func largestPrimeFactor(n int) int {
-	var largest int
+	largest := 0
 
-	// Remove all the even factors first
+	// Find and remove all factors of 2
 	for n%2 == 0 {
 		largest = 2
 		n /= 2
 	}
 
-	// Now, n must be an odd number
-	for i := int(3); i <= int(math.Sqrt(float64(n))); i += 2 {
-		for n%i == 0 {
-			largest = i
-			n /= i
+	// Start with the next potential prime factor
+	factor := 3
+	for factor*factor <= n {
+		if n%factor == 0 {
+			largest = factor
+			n /= factor
+		} else {
+			// Move to the next odd number
+			factor += 2
 		}
 	}
 
-	// If n is still greater than 2, it's the largest prime factor
-	if n > 2 {
+	// If n is still greater than 1, it's the largest prime factor
+	if n > 1 {
 		largest = n
 	}
 
@@ -32,8 +35,9 @@ func largestPrimeFactor(n int) int {
 
 func main() {
 	var num int
-	fmt.Println("enter the  number ")
+	fmt.Print("Enter a number: ")
 	fmt.Scanln(&num)
+
 	largest := largestPrimeFactor(num)
 	fmt.Printf("The largest prime factor of %d is: %d\n", num, largest)
 }
