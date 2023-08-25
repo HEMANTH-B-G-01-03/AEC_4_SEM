a3c116ea969c7c7b8a4d1638534c95d9eab4ec60
HEMANTH <110120035+HEMANTH-B-G-01-03@users.noreply.github.com>
2023-08-25 19:48:32 +0530

    Add files via upload

---
+++ EvenFibo.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var limit int
+	fmt.Println(" enter the limit")
+	fmt.Scanln(&limit)
+	//limit := 400 // Set your desired limit here
+	sum := 0
+	a := 1
+	b := 2
+
+	for b <= limit {
+		if b%2 == 0 {
+			sum += b
+		}
+		a, b = b, a+b
+	}
+
+	fmt.Printf("The sum of even Fibonacci numbers up to %d is: %d\n", limit, sum)
+}
